d8a7e37cafb3cc6a1cc8f042a11e717a3308c125
Vignesh Kumar M <vigneshkumar249@gmail.com>
2021-12-08 23:38:34 -0800

    Added files for Social media analytics project

---
+++ auth.py
@@ -0,0 +1,5 @@
+class TwitterAuth:
+    consumer_key = 'OJcP5rGQClsQTWUyYnE1vuMt9'
+    consumer_secret = 'hC5lFiPlE0hnEJ34TVUSqmDyJcf07yrCD3Yk9TKY3DaBB2JQrY'
+    access_token = '541722881-yzuPHfU9fMWoDZr98HBoVVCSVAxBLFvHqV2dyUSu'
+    access_token_secret = 'sqv5E0VN4G4DPVzuMMKBbGE30scjPsu16JvH9gIebZnuh'
